ac909b0d93057cbbba83eb1b63cc59901e97a1c3
James Russel C. Bautista <jrussclay@gmail.com>
2022-08-01 20:43:32 +0800

    Add products api endpoint

--- store/views.py
+++ store/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .serializers import ProductSerializer
+from .models import Product
+
+
+@api_view()
+def product_list(request):
+    query_set = Product.objects.all()
+    serializer = ProductSerializer(instance=query_set, many=True)
+    return Response(serializer.data)
+
+
+@api_view()
+def product_detail(request, id):
+    try:
+        product = Product.objects.get(pk=id)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    except Product.DoesNotExist:
+        return Response(data={"detail": "Not found"}, status=status.HTTP_404_NOT_FOUND)
--- storefront/settings.py
+++ storefront/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "store",
     "tags",
     "likes",
@@ -82,7 +83,7 @@ DATABASES = {
         "NAME": "storefront",
         "HOST": "localhost",
         "USER": "root",
-        "PASSWORD": "password"
+        "PASSWORD": "password",
     }
 }
 
@@ -127,3 +128,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {"COERCE_DECIMAL_TO_STRING": False}
