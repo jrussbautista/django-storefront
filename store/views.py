a07ce98da187721ed084f627ebc6670f7f148b73
James Russel C. Bautista <jrussclay@gmail.com>
2022-08-21 21:34:01 +0800

    Add generic filter for products

--- store/views.py
+++ store/views.py
@@ -1,28 +1,23 @@
-from itertools import count
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
+from .filters import ProductFilter
 
 from .serializers import ProductSerializer
 from .models import OrderItem, Product
 
 
 class ProductViewSet(ModelViewSet):
-
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ProductFilter
 
     def get_serializer_context(self):
         return {"request": self.request}
-
-    def get_queryset(self):
-        queryset = Product.objects.all()
-        collection_id = self.request.query_params.get("collection_id")
-        if collection_id is not None:
-            queryset = queryset.filter(collection_id=collection_id)
-        return queryset
 
     def destroy(self, request, *args, **kwargs):
         if OrderItem.objects.filter(product_id=kwargs["pk"]).count() > 0:
