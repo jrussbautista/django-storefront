c6b2dfdd9e4f4aaf3cef642ab38413000a6569ef
James Russel C. Bautista <jrussclay@gmail.com>
2022-08-21 21:54:24 +0800

    Add pagination

--- store/views.py
+++ store/views.py
@@ -1,19 +1,23 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
+
+from .pagination import DefaultPagination
 from .filters import ProductFilter
-from .serializers import ProductSerializer
 from .models import OrderItem, Product
+from .serializers import ProductSerializer
 
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
+    pagination_class = DefaultPagination
     search_fields = ["title", "description"]
+    ordering_fields = ["price", "last_update"]
 
     def get_serializer_context(self):
         return {"request": self.request}
--- storefront/settings.py
+++ storefront/settings.py
@@ -130,4 +130,6 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-REST_FRAMEWORK = {"COERCE_DECIMAL_TO_STRING": False}
+REST_FRAMEWORK = {
+    "COERCE_DECIMAL_TO_STRING": False,
+}
