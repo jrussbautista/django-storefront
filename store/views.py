9097d461195aa17e212a977aee931cd967f9075c
James Russel C. Bautista <jrussclay@gmail.com>
2022-08-21 21:46:07 +0800

    Add search for products

--- store/views.py
+++ store/views.py
@@ -1,11 +1,9 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.decorators import api_view
+from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
-
 from .filters import ProductFilter
-
 from .serializers import ProductSerializer
 from .models import OrderItem, Product
 
@@ -13,8 +11,9 @@ from .models import OrderItem, Product
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_class = ProductFilter
+    search_fields = ["title", "description"]
 
     def get_serializer_context(self):
         return {"request": self.request}
