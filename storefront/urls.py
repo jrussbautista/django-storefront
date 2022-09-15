1e61dbdc4038923dda1bccbcbcd6c06f984e5a1c
James Russel C. Bautista <jrussclay@gmail.com>
2022-09-15 21:41:11 +0800

    Add registers user feature

--- storefront/urls.py
+++ storefront/urls.py
@@ -16,4 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("store/", include("store.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("store/", include("store.urls")),
+    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.jwt")),
+]
