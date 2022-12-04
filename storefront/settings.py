3b93c0c5c3a2c7717623b3640f72f859c7870fd6
James Russel C. Bautista <jrussclay@gmail.com>
2022-12-04 11:16:33 +0800

    Add cors

--- storefront/settings.py
+++ storefront/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django_filters",
     "rest_framework",
     "djoser",
+    "corsheaders",
     "store",
     "tags",
     "likes",
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -155,3 +157,5 @@ DJOSER = {
 }
 
 SIMPLE_JWT = {"AUTH_HEADER_TYPES": ("JWT",), "ACCESS_TOKEN_LIFETIME": timedelta(days=1)}
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:8001", "http://127.0.0.1:8001"]
