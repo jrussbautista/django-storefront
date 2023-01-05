1a3193cdd521d73d05c3a1ac35d750779a6fc24e
James Russel C. Bautista <jrussclay@gmail.com>
2023-01-05 21:50:50 +0800

    Setup celery for background tasks

--- playground/views.py
+++ playground/views.py
@@ -1,20 +1,21 @@
 from django.core.mail import EmailMessage, BadHeaderError
 from templated_mail.mail import BaseEmailMessage
 from django.http import JsonResponse
+from .tasks import notify_customers
 
 
 def send_email(request):
-    try:
-        # message = EmailMessage(
-        #     "subject", "message", "info@storefront.com", ["johndoe@gmail.com"]
-        # )
-        # message.attach_file("playground/static/images/dog.jpeg")
-        # message.send()
-        message = BaseEmailMessage(
-            template_name="emails/hello.html", context={"name": "John Doe"}
-        )
-        message.send(["janedoe@gmail.com"])
-    except BadHeaderError:
-        pass
+    notify_customers.delay("Hello")
+    # try:
+    #     # message = EmailMessage(
+    #     #     "subject", "message", "info@storefront.com", ["johndoe@gmail.com"]
+    #     # )
+    #     # message.attach_file("playground/static/images/dog.jpeg")
+    #     # message.send()
+    #     message = BaseEmailMessage(
+    #         template_name="emails/hello.html", context={"name": "John Doe"}
+    #     )
+    #     message.send(["janedoe@gmail.com"])
+    # except BadHeaderError:
+    #     pass
     return JsonResponse({"message": "Email sent"})
- 
--- storefront/settings.py
+++ storefront/settings.py
@@ -167,3 +167,5 @@ EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
 EMAIL_PORT = 2525
 DEFAULT_FROM_EMAIL = "from@storefront.com"
+
+CELERY_BROKER_URL = "redis://localhost:6379/1"
